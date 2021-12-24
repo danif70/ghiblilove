f74768026565ee68f8f0534f9bb536af89bd59fa
keiry <keiryquero@gmail.com>
2021-12-24 10:50:29 -0500

    se creo las funcion de registro y LogIn de usuario, se creo la configuracion de firebase en el proyecto

--- src/Components/LoginRegister.jsx
+++ src/Components/LoginRegister.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { register, logIn } from '../firebase/functionsFirebase'
 
 const LoginRegister = () => {
   return (
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBSSYwUycDJL-hLgSnZHvbWR9ti_sbZPqs",
+    authDomain: "ghiblilove-462e8.firebaseapp.com",
+    projectId: "ghiblilove-462e8",
+    storageBucket: "ghiblilove-462e8.appspot.com",
+    messagingSenderId: "508407818791",
+    appId: "1:508407818791:web:0905b3ecb23d919b45bc02"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  //https://ghiblilove-462e8.firebaseapp.com/__/auth/handler
