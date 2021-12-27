fe37eb704b8d15d27c82bbc4602bd8c74537ee0c
keiry <keiryquero@gmail.com>
2021-12-27 17:56:40 -0500

    instalacion de bootstrap, renderizado de formulario registro,

--- src/Components/LoginRegister.jsx
+++ src/Components/LoginRegister.jsx
@@ -1,51 +1,112 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Fragment } from 'react'
-import { register, logIn } from '../firebase/functionsFirebase'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginRegister = () => {
+
+  const [email,setEmail]= useState('');
+  const [password,setPassword] = useState('');
+  const [userName,setUserName]= useState('');
+
+//Funcion para registro de usuarios nuevos
+const register = ()=>{
+  console.log(userName, email, password);
+  /* const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    }); */
+} 
+
+//Funcion para ingreso de usuarios registrados
+/* const logIn = () =>{
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+} 
+ */
+
   return (
     <Fragment>
       <p>login</p>
-      <Form className="form-login">
+     {/*  <Form className="form-login">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Control 
+          type="email" 
+          placeholder="Enter email"
+          value = {email}
+          onChange={(e)=>setEmail(e.target.value)}
+          />          
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control 
+          type="password" 
+          placeholder="Password"
+          value = {password} 
+          onChange={(e)=>setPassword(e.target.value)}
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button 
+        variant="primary" 
+        type="submit"
+        onClick={()=>logIn(email,password)}>
           Submit
         </Button>
-    </Form>
+    </Form> */}
     <p>Register</p>
       <Form className="form-register">
+      <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>User</Form.Label>
+          <Form.Control 
+          type="Text" 
+          placeholder="User Name" 
+          value = {userName} 
+          onChange={(e)=>setUserName(e.target.value)}
+          />
+        </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Control 
+          type="email" 
+          placeholder="Enter email"
+          value = {email} 
+          onChange={(e)=>setEmail(e.target.value)}
+          />          
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
+          <Form.Control 
+          type="password" 
+          placeholder="Password" 
+          value = {password} 
+          onChange={(e)=>setPassword(e.target.value)}
+          />
+        </Form.Group>        
+        <Button 
+        variant="primary" 
+        type="submit"
+        onClick={()=>register(userName,email,password)}>
           Submit
         </Button>
     </Form>
