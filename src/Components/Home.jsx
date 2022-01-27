a86537cda65bfc34f77c8213f587552a2f3a7e90
Daniela Funes <danielafunes1970@gmail.com>
2022-01-27 15:19:35 -0500

    Conexion con Api de Ghibli

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <p>Home</p>
-    </div>
-  )
-}
+  const [data, setData] = useState("");
+  useEffect(() => {
+    const url = "https://ghibliapi.herokuapp.com/films";
+    const fn = async () => {
+      const response = await fetch(url, {
+        method: "GET",
+      });
+      const data = await response.json();
+      setData(data);
+      //console.log(data.map(film => film.title))
+      console.log(data);
+    };
+    fn();
+  }, []);
 
-export default Home
+  return (
+    <Fragment>
+    <div >
+      {data &&
+        data.map((film) => (
+          <div key= {film.id}>
+            <p>{film.title}</p>
+            <img src={film.movie_banner} alt="movie banner"></img>
+          </div>
+          
+
+        ))}
+        
+      {/* data && comprueba q la data existe antes de renderizarla */}
+    </div>
+    </Fragment>
+  );
+};
+
+export default Home;
